66cee2b472b6bb9c6873e6619b2c9bc0252a538b
Rylee Rosenberger <rrosenberger@uvic.ca>
2024-09-30 12:19:04 -0700

    a1 parts 1 and 2 working

--- a1.c
+++ a1.c
@@ -49,9 +49,6 @@ char* get_hn(){
 void tokenize_args(char usr_input[], char* args[]){
     char *token;
     int num_tokens = 0;
-
-    //remove newline
-    usr_input[strcspn(usr_input, "\n")] = '\0';
     
     //tokenize and store tokens in args[]
     token = strtok(usr_input, " ");
@@ -68,47 +65,98 @@ void tokenize_args(char usr_input[], char* args[]){
     }
 }
 
-void part_1(){
-    char *cwd = get_directory();
-    char *usr = getlogin();
-    char *host = get_hn();
+void part2(char *args[]){
+
+    char *dest = args[1]; 
+    char *home = getenv("HOME");
     
-    char usr_input[std_sz]; //declare buffer for storing user input
-    char *args[max_args]; //to store args from input
-    pid_t pid;
+    if (dest==NULL || strcmp(dest, "~") == 0){
+        //go to home
+        chdir(home);
 
-    //print usr@host: cwd > and then get commands from user
-    printf("%s@%s: %s > ", usr, host, cwd);
-    fgets(usr_input, sizeof(usr_input), stdin);
+    } else {
+        //go to dest
+        if (chdir(dest)==0){
+            // chdir successful
+            return;
+        
+        } else {
+            perror("chdir failed");
+            exit(1);
+        }
+    }
+    return;
+}
 
-    //tokenize the input
-    tokenize_args(usr_input, args);
-
+void part1(char *args[]){
     //fork and execute
+    pid_t pid;
     pid = fork();
+    
     if (pid < 0) {
         //fork failed
-        perror("part 1 fork failed");
+        perror("fork failed");
         exit(1);
+    
     } else if (pid == 0) {
         //child process - execute user commands
+        
         execvp(args[0], args);
 
         //following line is executed if execvp fails
         perror("execvp failed");
     } else {
-        //parent process - wait for child to finish
-        //is this needed though?
+        //parent process - wait for child to finish so that child does not enter 'zombie state'
         wait(NULL);
 
     }
-    //free dynamically allocated memory - could maybe do this at the end of main?
+}
+
+
+void run_shell(){
+    char usr_input[std_sz]; //declare buffer for storing user input
+    char *args[max_args]; //to store args from input
+    const char *ex = "exit";
+
+    
+    char *usr = getlogin();
+    char *host = get_hn();
+    char *cwd = get_directory();
+
+    //print usr@host: cwd > and then get commands from user
+    while(1){
+        //open command line for user input
+        cwd = get_directory();
+        printf("%s@%s: %s > ", usr, host, cwd);
+        fgets(usr_input, sizeof(usr_input), stdin);
+
+        //remove newline from user input
+        usr_input[strcspn(usr_input, "\n")] = '\0';
+
+        //check for "exit" command
+        if(strcmp(ex, usr_input)==0){
+            break;
+        }
+        //tokenize the input
+        tokenize_args(usr_input, args);
+        
+        if (strcmp(args[0], "cd")==0){
+            part2(args);
+        } else {
+            part1(args);
+            
+        }
+    }
     free(cwd);
     free(host);
+    return;
+    
+    
 }
 
 //remember to free cwd and host since malloc was used!
 int main(){
-    part_1();
+    run_shell();
+    
     return 0;
 }
